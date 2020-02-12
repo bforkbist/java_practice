5ecaae6505b35c388735178e0d3f86efbc488e72
bforkbist <bforkbist@gmail.com>
2020-02-12 21:25:25 +0530

    really don't know the changes

--- java_practice/data_structure.java
+++ java_practice/data_structure.java
@@ -7,41 +7,45 @@ import java.util.Scanner;
 class data_structure {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // array 
-        // creating and initializing 
-        int[] a;
-        char b[]= new char[20];
-        a = new int[10];
-        String s= in.next();
-        b=s.toCharArray();
-        for( int j = 0;j < a.length;j++){
-            a[j]= Integer.parseInt(String.valueOf(b[j]));
-            System.out.println(a[j]);
-        }
-        // Strings 
-        String s=in.next(); //just commentout above part for s and b
-        char[] d=s.toCharArray();
-        String b = new String(d);
-        System.out.println(s);
-        System.out.println(d);
-        System.out.println(b);
+        // // array 
+        // // creating and initializing 
+        // int[] a;
+        // char b[]= new char[20];
+        // a = new int[10];
+        // String s= in.next();
+        // b=s.toCharArray();
+        // for( int j = 0;j < a.length;j++){
+        //     a[j]= Integer.parseInt(String.valueOf(b[j]));
+        //     System.out.println(a[j]);
+        // }
+        // // Strings 
+        // String s=in.next(); //just commentout above part for s and b
+        // char[] d=s.toCharArray();
+        // String b = new String(d);
+        // System.out.println(s);
+        // System.out.println(d);
+        // System.out.println(b);
 
-        // hashmap
-        HashMap< Integer, String > map= new HashMap<>();
-        for (int i = 0; i < 2; i++) {
-            map.put(in.nextInt(), in.next());
-        }
-        for (int i : map.keySet()) {
-            System.out.println(i + " : " + map.get(i));
-        }
-        System.out.println(map.size());
-        System.out.println(map.isEmpty());
-        System.out.println(map.containsValue("aditya"));
-        System.out.println(map);
-        System.out.println(map.remove(1));
-        System.out.println(map);
-        map.clear();
-        System.out.println(map);
+        // // hashmap
+        // HashMap< Integer, String > map= new HashMap<>();
+        // for (int i = 0; i < 2; i++) {
+        //     map.put(in.nextInt(), in.next());
+        // }
+        // for (int i : map.keySet()) {
+        //     System.out.println(i + " : " + map.get(i));
+        // }
+        // System.out.println(map.size());
+        // System.out.println(map.isEmpty());
+        // System.out.println(map.containsValue("aditya"));
+        // System.out.println(map);
+        // System.out.println(map.remove(1));
+        // System.out.println(map);
+        // map.clear();
+        // System.out.println(map);
+        
+
+        // // linkedlist
+
         in.close();
     }
 }
